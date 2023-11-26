5442fd7d4315f59b915fcaa246a2d67ee6d9f7fa
mmm <mull_ma@163.com>
2023-11-26 17:12:28 +0800

    -ALGPLinker

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ from utils.train_utils import load_model_and_parallel, build_optimizer_and_sched
 from utils.metric_utils import calculate_metric_relation, get_p_r_f
 from tensorboardX import SummaryWriter
 
+import time
+
 args = config.Args().get_parser()
 set_seed(args.seed)
 logger = logging.getLogger(__name__)
@@ -43,6 +45,7 @@ class BertForRe:
         self.model.zero_grad()
         eval_steps = args.eval_steps #每多少个step打印损失及进行验证
         best_f1 = 0.0
+        start_time = time.time()
         for epoch in range(self.args.train_epochs):
             for step, batch_data in enumerate(self.train_loader):
                 self.model.train()
@@ -60,10 +63,11 @@ class BertForRe:
                 self.optimizer.step()
                 self.scheduler.step()
                 self.model.zero_grad()
-                logger.info('【train】 epoch:{} {}/{} loss:{:.4f} entity_loss:{:.4f} head_loss:{:.4f} tail_loss:{:.4f}'.format(
-                  epoch, global_step, self.t_total, loss.item(), entity_loss.item(), head_loss.item(), tail_loss.item()))
+                elapsed = time.time() - start_time
+                logger.info('【train】 epoch:{} {}/{} loss:{:.4f} entity_loss:{:.4f} head_loss:{:.4f} tail_loss:{:.4f} speed: {:5.2f}ms/b'.format(
+                  epoch, global_step, self.t_total, loss.item(), entity_loss.item(), head_loss.item(), tail_loss.item(),elapsed))
                 global_step += 1
-              
+                start_time = time.time()
                 if self.args.use_tensorboard == "True":
                     writer.add_scalar('data/loss', loss.item(), global_step)
                 if global_step % eval_steps == 0:
